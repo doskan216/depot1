04af53143ab46de300b6c03068a6757c9603d29e
doskan216 <you@example.com>
2015-12-08 07:37:47 +0000

    カタログの表示

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,15 @@
 class Product < ActiveRecord::Base
-     validates :title, :description, :image_url, presence: true
-     validates :price, numericality: {greater_than_or_equal_to: 0.01}
-     validates :title, uniqueness: true
-     validates :image_url, allow_blank: true, format: {
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
        with:    %r{\.(gif|jpg|png)\Z}i,
        message: 'はGIF、JPG、PNG画像のURLでなければなりません'
-     }
-   end
+    }
 
-#   validates :title, :description, :image_url, presence: true
-#   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-#   validates :title, uniqueness: true
-#   validates :image_url, allow_blank: true, format: {
-#     with:    %r{\.(gif|jpg|png)$}i,
-#     message: 'must be a URL for GIF, JPG or PNG image.'
-#   }
-# end
+     #自由課題
+#商品名が10文字以上の長さであることを調べる
+#検証に関連づけたエラーメッセージの一つを変更する。
+    validates :title, length:{minimum: 10, message: "は10文字以上です"}
+
+   end
