bed8bcdcc756a5cf7e0b9b4662da945b701b6a2c
doskan216 <you@example.com>
2015-12-09 06:23:50 +0000

    カートの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,15 @@ class ApplicationController < ActionController::Base
     @time = Time.now
   end
 
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
+
 end
