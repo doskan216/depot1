f95cf4cb6c92ff218f09509f393b7460be54c003
doskan216 <you@example.com>
2015-12-10 02:20:00 +0000

    もっとスマートなカート終了

--- db/migrate/20151209070449_combine_items_in_cart.rb
+++ db/migrate/20151209070449_combine_items_in_cart.rb
@@ -19,5 +19,15 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
 
+  def down
+    #数量＞1の品目を複数の品目に分割する
+    LineItem.where("quantity").each do |line_item|
+    #個別の品目を追加する
+    line_item.quantity.times doLineItem.create cart_id: line_item.cart_id,
+      product_id: line_item.product_id, quantity: 1
+    end
+    #元の品目を削除する
+    line_item.destroy
+  end
 
 end
